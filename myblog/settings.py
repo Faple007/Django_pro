83cb1a796c6d6cb51800c222593b13deb7936c17
Faple007 <st0963105799@gmail.com>
2018-08-17 17:22:04 +0800

    update

--- myblog/settings.py
+++ myblog/settings.py
@@ -18,6 +18,9 @@ DISABLE_COLLECTSTATIC=1
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATIC_TMP = os.path.join（BASE_DIR，'static'）
+os.makedirs（STATIC_TMP，exist_ok = True）
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
