cf4fceb0d67b4569a6c085f4ae325760756f3424
Faple007 <st0963105799@gmail.com>
2018-08-17 17:18:02 +0800

    update

--- myblog/settings.py
+++ myblog/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import dj_database_url
 import os
 
+
+DISABLE_COLLECTSTATIC=1
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
